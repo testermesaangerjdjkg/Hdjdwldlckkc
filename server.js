49e9045433342768e23004dbfabe1974364795b7
testermesaangerjdjkg <talievflytomaru@gmail.com>
2026-02-20 00:29:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,22 +2,24 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
-const cors = require('cors');
 
 const app = express();
-app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: "*", methods: ["GET", "POST"] }
-});
+const io = new Server(server);
 
 io.on('connection', (socket) => {
+  // Обмен ключами
+  socket.on('dh-key-exchange', (data) => {
+    socket.broadcast.emit('dh-key-exchange', data);
+  });
+
+  // Пересылка сообщений
   socket.on('chat message', (data) => {
     socket.broadcast.emit('chat message', data);
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+server.listen(PORT, () => console.log(`Freedom Premium on port ${PORT}`));
